3f13e0ce06670f28063f6b0de13871759376b98f
Poorna satya sai ram Darla <poornasatyasairamdarla@Poornas-MacBook-Air.local>
2023-12-22 10:43:56 +0530

    initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('My Courseaaaa') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="">
+            <div class="">
+                <x-welcome />
+            </div>
+        </div>
+    </div>
+</x-app-layout>
